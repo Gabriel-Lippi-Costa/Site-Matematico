e166d0f39fd94f9d2293a8e43dfd2e7c499f03b0
Gabriel Lippi <gabriellippidacosta@gmail.com>
2025-01-15 15:56:36 -0300

    .

--- matematica_financeira/js.js
+++ matematica_financeira/js.js
@@ -1,6 +1,7 @@
 function calcularjs() {
     var c = Number(window.document.getElementById('ic-js').value)
     var i = Number(window.document.getElementById('ii-js').value)
+    i = i / 100
     var t = Number(window.document.getElementById('it-js').value)
     var res = window.document.getElementById('res-js')
     var Resultado = c * i * t
